f227adefddaa8f9e6221c29aef70c13e042f1bb9
makkenno <kota.m.2774@gmail.com>
2023-05-14 20:47:57 +0900

    feat: 料理提案ページの実装

---
+++ frontend/src/pages-layout/Recipes/Recipes.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { SPStory } from "@/lib/storybook/SPStory";
+import { recipeCardArgs } from "@/lib/storybook/recipeCardArgs";
+import { Recipes } from "@/pages-layout/Recipes/Recipes";
+
+const meta: Meta<typeof Recipes> = {
+  title: "Page-layout/Recipes",
+  component: Recipes,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Recipes>;
+
+export const Default: Story = {
+  parameters: {
+    ...SPStory.parameters,
+  },
+  args: recipeCardArgs,
+};
+export const PCDefault: Story = {
+  args: recipeCardArgs,
+};
---
+++ frontend/src/pages-layout/Recipes/Recipes.tsx
@@ -0,0 +1,38 @@
+import { type FC } from "react";
+import { Anchor, Box, Flex, Header, Space } from "@mantine/core";
+import { ActionButton } from "@/component/ActionButton/ActionButton";
+import {
+  RecipeCard,
+  type RecipeCardProps,
+} from "@/pages-component/Recipes/RecipeCard/RecipeCard";
+
+type RecipeProps = RecipeCardProps;
+
+export const Recipes: FC<RecipeProps> = ({
+  name,
+  imageUrl,
+  recipe,
+  onClick,
+}) => {
+  return (
+    <main>
+      <Box pt={48} px="xs" pb={56}>
+        <Header height={24}>
+          <Anchor>気分を入れ直す</Anchor>
+        </Header>
+        <Space h="lg" />
+        <RecipeCard
+          name={name}
+          imageUrl={imageUrl}
+          recipe={recipe}
+          onClick={onClick}
+        />
+        <Space h="xl" />
+        <Flex gap="xs">
+          <ActionButton>レシピを見る</ActionButton>
+          <ActionButton>別の料理を見る</ActionButton>
+        </Flex>
+      </Box>
+    </main>
+  );
+};
