275e5ee9df68b851f5ec998f7719e7907fa2acd9
t5a1r1ou <baw.keepon69@gmail.com>
2023-05-15 22:21:28 +0900

    refactor: フォントサイズの指定でthemeを使う

--- frontend/src/component/RecipeDetails/RecipeDetails.tsx
+++ frontend/src/component/RecipeDetails/RecipeDetails.tsx
@@ -1,5 +1,5 @@
 import { type FC } from "react";
-import { List, rem, Text, Title } from "@mantine/core";
+import { List, Text, Title, useMantineTheme } from "@mantine/core";
 
 type RecipeDetailsProps = {
   /**
@@ -24,60 +24,63 @@ export const RecipeDetails: FC<RecipeDetailsProps> = ({
   ingredients,
   steps,
   tips,
-}) => (
-  <>
-    <Title order={2} size={16} mb={4}>
-      【材料】
-    </Title>
-    <List
-      spacing={4}
-      mb={24}
-      icon={"・"}
-      styles={(_) => ({
-        itemIcon: {
-          fontSize: rem(16),
-          fontWeight: 600,
-          marginRight: 0,
-        },
-      })}
-    >
-      {ingredients.map((ingredient) => (
-        <List.Item key={ingredient.name}>
-          <Text size={16} fw={600}>
-            {ingredient.name}({ingredient.amount})
-          </Text>
-        </List.Item>
-      ))}
-    </List>
-    <Title order={2} size={16} mb={4}>
-      【作り方】
-    </Title>
-    <List
-      spacing={4}
-      mb={24}
-      type={"ordered"}
-      styles={(_) => ({
-        item: {
-          "&::marker": {
-            fontSize: rem(16),
+}) => {
+  const theme = useMantineTheme();
+  return (
+    <>
+      <Title order={2} size={theme.fontSizes.xs} mb={4}>
+        【材料】
+      </Title>
+      <List
+        spacing={4}
+        mb={24}
+        icon={"・"}
+        styles={(_) => ({
+          itemIcon: {
+            fontSize: theme.fontSizes.xs,
             fontWeight: 600,
+            marginRight: 0,
           },
-        },
-      })}
-    >
-      {steps.map((step) => (
-        <List.Item key={step}>
-          <Text size={16} fw={600}>
-            {step}
-          </Text>
-        </List.Item>
-      ))}
-    </List>
-    <Title order={2} size={16}>
-      【tips】
-    </Title>
-    <Text size={16} fw={600}>
-      {tips.join("")}
-    </Text>
-  </>
-);
+        })}
+      >
+        {ingredients.map((ingredient) => (
+          <List.Item key={ingredient.name}>
+            <Text size={theme.fontSizes.xs} fw={600}>
+              {ingredient.name}({ingredient.amount})
+            </Text>
+          </List.Item>
+        ))}
+      </List>
+      <Title order={2} size={theme.fontSizes.xs} mb={4}>
+        【作り方】
+      </Title>
+      <List
+        spacing={4}
+        mb={24}
+        type={"ordered"}
+        styles={(_) => ({
+          item: {
+            "&::marker": {
+              fontSize: theme.fontSizes.xs,
+              fontWeight: 600,
+            },
+          },
+        })}
+      >
+        {steps.map((step) => (
+          <List.Item key={step}>
+            <Text size={theme.fontSizes.xs} fw={600}>
+              {step}
+            </Text>
+          </List.Item>
+        ))}
+      </List>
+      <Title order={2} size={theme.fontSizes.xs}>
+        【tips】
+      </Title>
+      <Text size={theme.fontSizes.xs} fw={600}>
+        {tips.join("")}
+      </Text>
+    </>
+  );
+};
